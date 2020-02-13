6a73429a7c7d5e598eecb4ffdd50de0ac050f812
Nasir Uddin <nasir.rubel99@gmail.com>
2020-02-13 22:18:13 +0600

    footer polish and address column to brand

---
+++ db/migrate/20200213161653_add_address_column_to_brand.rb
@@ -0,0 +1,5 @@
+class AddAddressColumnToBrand < ActiveRecord::Migration[6.0]
+  def change
+    add_column :brands, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_211709) do
+ActiveRecord::Schema.define(version: 2020_02_13_161653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_211709) do
     t.string "linkedin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "media_items", force: :cascade do |t|
